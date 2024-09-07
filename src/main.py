9d0221d489a38619246c391d087191efd094b0f6
shashank <shas.srivastava@gmail.com>
2024-09-07 18:19:56 +0800

    Load model and predict

--- src/main.py
+++ src/main.py
@@ -2,11 +2,11 @@
 # Library Imports
 import os
 import numpy as np
-import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Activation
 from tensorflow.keras.losses import CategoricalCrossentropy
 from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.models import load_model
 
 # Define data file path
 this_dir, this_filename = os.path.split(__file__)
@@ -55,3 +55,36 @@ model.add(Activation('softmax'))
 model.compile(loss=CategoricalCrossentropy(), optimizer=RMSprop(learning_rate=0.01))
 model.fit(x, y, batch_size=256, epochs=10)
 model.save("../Models/RedHotLyrics_v1.keras")
+
+# Load trained model
+trained_model = load_model("../Models/RedHotLyrics_v1.keras")
+
+# Predict
+def sample(preds, temperature=1.0):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+def generate_text(length, temperature):
+    start_index = np.random.randint(0, len(input_seqs) - SEQUENCE_LENGTH - 1)
+    generated = ['']
+    sentence = input_seqs[start_index: start_index + SEQUENCE_LENGTH]
+    generated += sentence
+    for i in range(length):
+        x = np.zeros((1, SEQUENCE_LENGTH, len(characters)))
+        for t, character in enumerate(sentence):
+            for c in character:
+                x[0, t, char_to_index[c]] = 1
+
+        predictions = model.predict(x, verbose=0)[0]
+        next_index = sample(predictions, temperature)
+        next_character = index_to_char[next_index]
+
+        generated += next_character
+        sentence = sentence[1:] + list(next_character)
+    return generated
+
+print(generate_text(100, 0.35))
