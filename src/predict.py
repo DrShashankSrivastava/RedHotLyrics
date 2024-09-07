30aaa754f03d96abaa3d864e007cd9cde3f8d306
shashank <shas.srivastava@gmail.com>
2024-09-07 18:27:12 +0800

    Re-format files

--- src/predict.py
+++ src/predict.py
@@ -1,11 +1,5 @@
-
-# Library Imports
 import os
 import numpy as np
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Activation
-from tensorflow.keras.losses import CategoricalCrossentropy
-from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.models import load_model
 
 # Define data file path
@@ -34,30 +28,12 @@ next_char = []
 
 # Form inputs and target for model training
 for i in range(0, len(data) - SEQUENCE_LENGTH, STEP_SIZE):
-    input_seqs.append(data[i:i+SEQUENCE_LENGTH])
-    next_char.append(data[i+SEQUENCE_LENGTH])
-
-# Define input and output placeholders
-x = np.zeros((len(input_seqs), SEQUENCE_LENGTH, len(characters)), dtype='bool')
-y = np.zeros((len(input_seqs), len(characters)), dtype='bool')
-
-# Prepare the input and output for ML
-for i, input_seq in enumerate(input_seqs):
-    for j, character in enumerate(input_seq):
-        x[i, j, char_to_index[character]] = 1
-    y[i, char_to_index[next_char[i]]] = 1
-
-# Create a Recurring Neural Network
-model = Sequential()
-model.add(LSTM(128, input_shape=(SEQUENCE_LENGTH, len(characters))))
-model.add(Dense(len(characters)))
-model.add(Activation('softmax'))
-model.compile(loss=CategoricalCrossentropy(), optimizer=RMSprop(learning_rate=0.01))
-model.fit(x, y, batch_size=256, epochs=10)
-model.save("../Models/RedHotLyrics_v1.keras")
+    input_seqs.append(data[i: i + SEQUENCE_LENGTH])
+    next_char.append(data[i + SEQUENCE_LENGTH])
 
 # Load trained model
 trained_model = load_model("../Models/RedHotLyrics_v1.keras")
+
 
 # Predict
 def sample(preds, temperature=1.0):
@@ -67,6 +43,7 @@ def sample(preds, temperature=1.0):
     preds = exp_preds / np.sum(exp_preds)
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
+
 
 def generate_text(length, temperature):
     start_index = np.random.randint(0, len(input_seqs) - SEQUENCE_LENGTH - 1)
@@ -86,5 +63,6 @@ def generate_text(length, temperature):
         generated += next_character
         sentence = sentence[1:] + list(next_character)
     return generated
+
 
 print(generate_text(100, 0.35))
---
+++ src/train_model.py
@@ -0,0 +1,56 @@
+
+# Library Imports
+import os
+import numpy as np
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Activation
+from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.optimizers import RMSprop
+
+# Define data file path
+this_dir, this_filename = os.path.split(__file__)
+DATA_PATH = os.path.join(this_dir, "../data", "lyrics.txt")
+
+# Read available lyrics
+with open(DATA_PATH, 'r') as file_obj:
+    data = file_obj.read().replace('\n', '')
+data = data.lower()
+
+# Extract all unique characters
+characters = sorted(set(data))
+
+# Convert char to int and vice-versa
+char_to_index = dict((c, i) for i, c in enumerate(characters))
+index_to_char = dict((i, c) for i, c in enumerate(characters))
+
+# Define model input length and step size
+SEQUENCE_LENGTH = 40
+STEP_SIZE = 5
+
+# Placeholders for inputs and targets
+input_seqs = []
+next_char = []
+
+# Form inputs and target for model training
+for i in range(0, len(data) - SEQUENCE_LENGTH, STEP_SIZE):
+    input_seqs.append(data[i:i+SEQUENCE_LENGTH])
+    next_char.append(data[i+SEQUENCE_LENGTH])
+
+# Define input and output placeholders
+x = np.zeros((len(input_seqs), SEQUENCE_LENGTH, len(characters)), dtype='bool')
+y = np.zeros((len(input_seqs), len(characters)), dtype='bool')
+
+# Prepare the input and output for ML
+for i, input_seq in enumerate(input_seqs):
+    for j, character in enumerate(input_seq):
+        x[i, j, char_to_index[character]] = 1
+    y[i, char_to_index[next_char[i]]] = 1
+
+# Create a Recurring Neural Network
+model = Sequential()
+model.add(LSTM(128, input_shape=(SEQUENCE_LENGTH, len(characters))))
+model.add(Dense(len(characters)))
+model.add(Activation('softmax'))
+model.compile(loss=CategoricalCrossentropy(), optimizer=RMSprop(learning_rate=0.01))
+model.fit(x, y, batch_size=256, epochs=10)
+model.save("../Models/RedHotLyrics_v1.keras")
